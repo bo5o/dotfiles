0136a3a2f325db960488fe9cec37f74bfa8bae2e
cbows <32486983+cbows@users.noreply.github.com>
2018-03-27 22:37:53 +0200

    Add i3, polybar and rofi configs.

---
+++ i3/.config/i3/lock.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Take a screenshot:
+scrot /tmp/screen.png
+
+# Create a blur on the shot:
+# convert /tmp/screen.png -paint 2 -swirl 360 /tmp/screen.png
+# convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+mogrify -scale 10% -scale 1000% /tmp/screen.png
+
+# Add the lock to the blurred image:
+[[ -f ~/.config/i3/lock.png ]] && convert /tmp/screen.png  ~/.config/i3/lock.png -gravity center -composite -matte /tmp/screen.png
+
+# Pause music (mocp and mpd):
+mpc pause
+
+# Lock it up!
+i3lock -e -f -c 000000 -i /tmp/screen.png
+
+# If still locked after 20 seconds, turn off screen.
+sleep 20 && pgrep i3lock && xset dpms force off
