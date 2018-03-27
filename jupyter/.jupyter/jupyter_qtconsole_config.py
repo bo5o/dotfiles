506236309e8d7a1b7fa0b10f14559c9d9c7f7aef
cbows <32486983+cbows@users.noreply.github.com>
2018-03-28 00:07:48 +0200

    Add ipython and jupyter configs.

---
+++ jupyter/.jupyter/jupyter_qtconsole_config.py
@@ -0,0 +1,6 @@
+c.JupyterQtConsoleApp.hide_menubar = True
+c.JupyterQtConsoleApp.display_banner = False
+
+c.ConsoleWidget.font_size = 12
+c.ConsoleWidget.font_family = 'mono'
+c.JupyterQtConsoleApp.stylesheet = '$HOME/.jupyter/my_stylesheet.css'
