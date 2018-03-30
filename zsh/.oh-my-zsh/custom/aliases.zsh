91c72fb265cb4367e7098fc2f9e4c120b71033ce
cbows <32486983+cbows@users.noreply.github.com>
2018-03-30 14:27:59 +0200

    Fix typo.

--- zsh/.oh-my-zsh/custom/aliases.zsh
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -45,4 +45,4 @@ alias da="cd ~/latex/diplomarbeit"
 alias Gst="gitcheck"
 alias disks='echo "╓───── m o u n t . p o i n t s"; echo "╙────────────────────────────────────── ─ ─ "; lsblk -a; echo ""; echo "╓───── d i s k . u s a g e"; echo "╙────────────────────────────────────── ─ ─ "; df -h;'
 alias dotfiles='cd ~/dotfiles'
-alias Dotflies='cd ~/private'
+alias Dotfiles='cd ~/private'
