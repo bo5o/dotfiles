4dc80da5fe37c606ff7e68158a89401d386e91d4
cbows <32486983+cbows@users.noreply.github.com>
2019-05-21 14:25:13 +0200

    scripts: Improve vim-anywhere.

--- scripts/.local/bin/vim-anywhere.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-#   Open vim to edit a temporary file and
-#   load content into clipboard afterwards.
-
-dir="/tmp/vim-anywhere/"
-fn="$(date -Iseconds)"
-[ ! -d $dir ] && mkdir $dir
-nvim $dir$fn && cat $dir$fn | xsel -b -i
