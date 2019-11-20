8a294d92ee0a42950a327551b82e10b28e69198d
cbows <32486983+cbows@users.noreply.github.com>
2019-11-20 11:48:16 +0100

    zsh: Add function to fuzzy open files.

--- zsh/.oh-my-zsh/custom/functions.zsh
+++ zsh/.oh-my-zsh/custom/functions.zsh
@@ -1,3 +1,5 @@
+# Like normal z when used with arguments but displays an fzf prompt when
+# used without.
 j() {
   if [[ -z "$*" ]]; then
     cd "$(_z -l 2>&1 | fzf +s --tac | sed 's/^[0-9,.]* *//')"
@@ -7,8 +9,23 @@ j() {
   fi
 }
 
+# Relaunch z with the arguments for the previous command
+# as the default input by using zz
 jj() {
   cd "$(_z -l 2>&1 | sed 's/^[0-9,.]* *//' | fzf -q "$_last_z_args")"
+}
+
+# Modified version where you can press
+#   - CTRL-O to open with `open` command,
+#   - CTRL-E or Enter key to open with the $EDITOR
+fo() {
+  local out file key
+  IFS=$'\n' out=("$(fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e)")
+  key=$(head -1 <<< "$out")
+  file=$(head -2 <<< "$out" | tail -1)
+  if [ -n "$file" ]; then
+    [ "$key" = ctrl-o ] && open "$file" || ${EDITOR:-vim} "$file"
+  fi
 }
 
 wetter()
