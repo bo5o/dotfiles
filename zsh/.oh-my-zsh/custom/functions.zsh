fc4c3d200c8d826e672ecf35594c998f6bb87870
cbows <32486983+cbows@users.noreply.github.com>
2019-11-20 11:31:13 +0100

    zsh: Add quickjump (combination of z and fzf).

--- zsh/.oh-my-zsh/custom/functions.zsh
+++ zsh/.oh-my-zsh/custom/functions.zsh
@@ -1,3 +1,16 @@
+j() {
+  if [[ -z "$*" ]]; then
+    cd "$(_z -l 2>&1 | fzf +s --tac | sed 's/^[0-9,.]* *//')"
+  else
+    _last_z_args="$@"
+    _z "$@"
+  fi
+}
+
+jj() {
+  cd "$(_z -l 2>&1 | sed 's/^[0-9,.]* *//' | fzf -q "$_last_z_args")"
+}
+
 wetter()
 {
     curl -H "Accept-Language: ${LANG%_*}" wttr.in/"${1:-Berlin}"
