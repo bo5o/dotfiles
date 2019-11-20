bc13e960c9ee843d0128d0047b5c50117fdc926e
cbows <32486983+cbows@users.noreply.github.com>
2019-11-20 14:33:28 +0100

    zsh: Improve fzf settings.

--- zsh/.oh-my-zsh/custom/fzf.zsh
+++ zsh/.oh-my-zsh/custom/fzf.zsh
@@ -4,14 +4,21 @@ export FZF_COMPLETION_TRIGGER=''
 bindkey '^T' fzf-completion
 bindkey '^I' $fzf_default_completion
 
-export FZF_DEFAULT_OPTS='
-    --color bw
-'
+export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
 
-export FZF_DEFAULT_COMMAND='
-    ag --hidden --ignore .git -f -g ""
-'
+export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -f -g ""'
 
 export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
 
+# Use fd (https://github.com/sharkdp/fd) instead of the default find
+# command for listing path candidates.
+# - The first argument to the function ($1) is the base path to start traversal
+# - See the source code (completion.{bash,zsh}) for the details.
+_fzf_compgen_path() {
+  fd --hidden --follow --exclude ".git" . "$1"
+}
 
+# Use fd to generate the list for directory completion
+_fzf_compgen_dir() {
+  fd --type d --hidden --follow --exclude ".git" . "$1"
+}
