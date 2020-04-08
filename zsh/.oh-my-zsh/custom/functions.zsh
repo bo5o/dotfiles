a2e8dd004dd528c3de49e616a5ecd591983b94f8
cbows <32486983+cbows@users.noreply.github.com>
2020-04-08 16:29:11 +0200

    zsh: Add function to quickly switch git branches.

--- zsh/.oh-my-zsh/custom/functions.zsh
+++ zsh/.oh-my-zsh/custom/functions.zsh
@@ -31,6 +31,13 @@ fo() {
   fi
 }
 
+# fbr - checkout git branch
+fbr() { local branches branch
+  branches=$(git --no-pager branch -vv) &&
+  branch=$(echo "$branches" | fzf +m) &&
+  git checkout $(echo "$branch" | awk '{print $1}' | sed "s/.* //")
+}
+
 wetter()
 {
     curl -H "Accept-Language: ${LANG%_*}" wttr.in/"${1:-Berlin}"
