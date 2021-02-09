c9c8eb98d470715ffda7068eca1d7a79a57b4091
cbows <32486983+cbows@users.noreply.github.com>
2021-02-09 17:48:54 +0100

    zsh: add helper to manage vimwiki git repo

--- zsh/.oh-my-zsh/custom/functions.zsh
+++ zsh/.oh-my-zsh/custom/functions.zsh
@@ -100,3 +100,15 @@ addreq() {
         done
     fi
 }
+
+vimwiki () {
+    if [[ $# == 0 ]]
+    then
+        nvim +'VimwikiIndex'
+    elif [[ $1 == 'git' ]]
+    then
+        git -C ~/vimwiki/ ${@:2}
+    else
+        echo 'Usage: vimwiki [git] [args ...]'
+    fi
+}
