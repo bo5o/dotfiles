731273afd6795210449d340059c5596d40557d77
cbows <32486983+cbows@users.noreply.github.com>
2021-11-17 03:05:14 +0100

    nvim: move most linters/fixers from ale to null-ls

--- nvim/.config/nvim/lua/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/null-ls.lua
@@ -2,7 +2,46 @@ local null_ls = require("null-ls")
 local builtins = null_ls.builtins
 
 null_ls.config({
-	sources = { builtins.formatting.stylua },
+	debounce = 250,
+	diagnostics_format = "[#{s}] #{c}: #{m}",
+	sources = {
+		-- Lua
+		builtins.formatting.stylua,
+		-- Python
+		builtins.diagnostics.flake8,
+		builtins.diagnostics.pylint,
+		builtins.diagnostics.mypy,
+		builtins.formatting.black,
+		builtins.formatting.isort,
+		-- Javascript/Typescript/Vue
+		builtins.diagnostics.eslint_d.with({
+			filetypes = { "javascript", "typescript", "vue" },
+		}),
+		builtins.formatting.eslint_d.with({
+			filetypes = { "javascript", "typescript", "vue" },
+		}),
+		-- CSS/HTML/JSON/YAML/Markdown
+		builtins.diagnostics.yamllint,
+		builtins.diagnostics.markdownlint.with({
+			filetypes = { "markdown", "vimwiki" },
+		}),
+		builtins.formatting.prettierd.with({
+			filetypes = { "css", "html", "json", "yaml", "markdown" },
+		}),
+		-- Dockerfile
+		builtins.diagnostics.hadolint,
+		-- Shell
+		builtins.diagnostics.shellcheck.with({
+			filetypes = { "sh", "zsh" },
+		}),
+		builtins.formatting.shfmt,
+		-- Go
+		builtins.formatting.gofmt,
+		-- Tex
+		builtins.diagnostics.chktex,
+		-- Vim
+		builtins.diagnostics.vint,
+	},
 })
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
