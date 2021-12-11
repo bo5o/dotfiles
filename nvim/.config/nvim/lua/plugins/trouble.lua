21a426969481f59932d9a87acdfa433fa788ea45
cbows <32486983+cbows@users.noreply.github.com>
2021-12-11 11:43:12 +0100

    nvim: update trouble config

--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -6,6 +6,8 @@ require("trouble").setup({
 	mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
 	fold_open = "", -- icon used for open folds
 	fold_closed = "", -- icon used for closed folds
+	group = true, -- group results by file
+	padding = true, -- add an extra new line on top of the list
 	action_keys = { -- key mappings for actions in the trouble list
 		-- map to {} to remove a mapping, for example:
 		-- close = {},
@@ -32,6 +34,7 @@ require("trouble").setup({
 	auto_close = false, -- automatically close the list when you have no diagnostics
 	auto_preview = false, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
 	auto_fold = false, -- automatically fold a file trouble list at creation
+	auto_jump = { "lsp_definitions" }, -- for the given modes, automatically jump if there is only a single result
 	signs = {
 		-- icons / text used for a diagnostic
 		error = "",
