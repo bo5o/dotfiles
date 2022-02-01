912d82c36e70001df6f53ef30a1f7482bc959205
cbows <32486983+cbows@users.noreply.github.com>
2022-02-01 18:36:39 +0100

    nvim: enable tresitter textobjects lookahead

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -34,6 +34,7 @@ require("nvim-treesitter.configs").setup({
 	textobjects = {
 		select = {
 			enable = true,
+			lookahead = true,
 			keymaps = {
 				["af"] = "@function.outer",
 				["if"] = "@function.inner",
