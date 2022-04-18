561a811e6dc1bb98c75d39bad7a52839d7cb5920
cbows <32486983+cbows@users.noreply.github.com>
2022-04-18 18:46:19 +0200

    nvim: use global status line

--- nvim/.config/nvim/lua/plugins/statusline.lua
+++ nvim/.config/nvim/lua/plugins/statusline.lua
@@ -6,6 +6,7 @@ require("lualine").setup({
 		section_separators = "",
 		disabled_filetypes = {},
 		always_divide_middle = true,
+		globalstatus = true,
 	},
 	sections = {
 		lualine_a = { "mode" },
