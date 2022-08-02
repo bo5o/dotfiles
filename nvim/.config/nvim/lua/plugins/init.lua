32e4a638653452ede68a8ac2510b79984527a451
cbows <32486983+cbows@users.noreply.github.com>
2022-08-02 17:42:16 +0200

    nvim: refactor floaterm config

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -626,22 +626,11 @@ return require("packer").startup(function(use)
       "FloatermNew",
       "FloatermSend",
     },
+    setup = function()
+      require("plugins.floaterm").setup()
+    end,
     config = function()
-      vim.g.floaterm_keymap_toggle = "<leader>ot"
-      vim.g.floaterm_keymap_prev = "<F2>"
-      vim.g.floaterm_keymap_next = "<F3>"
-      vim.g.floaterm_keymap_new = "<leader>oT"
-      vim.g.floaterm_width = 0.8
-      vim.g.floaterm_height = 0.8
-      vim.g.floaterm_wintitle = 0
-      vim.g.floaterm_autoclose = 1
-      vim.g.floaterm_opener = "edit"
-
-      vim.api.nvim_set_hl(
-        0,
-        "FloatermBorder",
-        { fg = "#ebdbb2", bg = nil, ctermfg = 0, ctermbg = 13 }
-      )
+      require("plugins.floaterm").config()
     end,
   })
 
