2e138de12835fcd347fe9480546e27558797f6fc
cbows <32486983+cbows@users.noreply.github.com>
2022-08-02 14:22:52 +0200

    nvim: dont show indent guides in mason buffer

--- nvim/.config/nvim/lua/plugins/indent_blankline.lua
+++ nvim/.config/nvim/lua/plugins/indent_blankline.lua
@@ -2,7 +2,14 @@ require("indent_blankline").setup({
   char = "┊",
   use_treesitter = true,
   show_trailing_blankline_indent = true,
-  filetype_exclude = { "help", "vimwiki", "markdown", "startify", "NvimTree" },
+  filetype_exclude = {
+    "help",
+    "vimwiki",
+    "markdown",
+    "startify",
+    "NvimTree",
+    "mason.nvim",
+  },
   buftype_exclude = { "terminal" },
   show_current_context = false,
 })
