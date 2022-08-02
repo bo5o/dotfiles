e02421c3e6224305c1cb919f971d888c81fce3b1
cbows <32486983+cbows@users.noreply.github.com>
2022-08-02 17:10:15 +0200

    nvim: refactor vim-dispatch config

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -476,13 +476,10 @@ return require("packer").startup(function(use)
     "tpope/vim-dispatch",
     cmd = { "Dispatch", "Make", "Focus", "Start" },
     setup = function()
-      vim.g.tmux_session = "popup"
+      require("plugins.vim_dispatch").setup()
     end,
     config = function()
-      local map = vim.keymap.set
-      map("n", "t<cr>", "<cmd>Tmux send-keys -t " .. vim.g.tmux_session .. " Enter<cr>")
-      map("n", "t<C-c>", "<cmd>Tmux send-keys -t " .. vim.g.tmux_session .. " C-c<cr>")
-      map("n", "t<C-d>", "<cmd>Tmux send-keys -t " .. vim.g.tmux_session .. " C-d<cr>")
+      require("plugins.vim_dispatch").config()
     end,
   })
 
