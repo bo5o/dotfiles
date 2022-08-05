1cceb32c0f95fdee424cef0ed0fbffa49c79cefb
cbows <32486983+cbows@users.noreply.github.com>
2022-08-05 12:23:18 +0200

    nvim: add python-specific settings for aerial.nvim

--- nvim/.config/nvim/lua/plugins/aerial_nvim.lua
+++ nvim/.config/nvim/lua/plugins/aerial_nvim.lua
@@ -3,8 +3,46 @@ local M = {}
 function M.config()
   -- require("aerial").on_attach(client, bufnr)
   require("aerial").setup({
-    backends = { "lsp", "treesitter" },
-    filter_kind = false,
+    backends = {
+      ["_"] = { "lsp", "treesitter" },
+      python = { "lsp" },
+    },
+    filter_kind = {
+      ["_"] = false,
+      python = {
+        -- "Array",
+        -- "Boolean",
+        "Class",
+        -- "Constant",
+        "Constructor",
+        "Enum",
+        "EnumMember",
+        -- "Event",
+        -- "Field",
+        -- "File",
+        "Function",
+        "Interface",
+        -- "Key",
+        "Method",
+        "Module",
+        -- "Namespace",
+        -- "Null",
+        -- "Number",
+        -- "Object",
+        -- "Operator",
+        -- "Package",
+        "Property",
+        -- "String",
+        "Struct",
+        -- "TypeParameter",
+        -- "Variable",
+      },
+    },
+    ignore = {
+      filetypes = {
+        "startify",
+      },
+    },
   })
 end
 
