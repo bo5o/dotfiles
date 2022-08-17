c90c6b3acdbbc105c06555cb8d7d03eb69810eb6
cbows <32486983+cbows@users.noreply.github.com>
2022-08-17 22:27:10 +0200

    nvim: update bufferline color definitions

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -25,19 +25,19 @@ function M.config()
           local hint = #vim.diagnostic.get(0, { severity = seve.HINT })
 
           if error ~= 0 then
-            table.insert(result, { text = "  " .. error, guifg = "#EC5241" })
+            table.insert(result, { text = "  " .. error, fg = "#EC5241" })
           end
 
           if warning ~= 0 then
-            table.insert(result, { text = "  " .. warning, guifg = "#EFB839" })
+            table.insert(result, { text = "  " .. warning, fg = "#EFB839" })
           end
 
           if hint ~= 0 then
-            table.insert(result, { text = "  " .. hint, guifg = "#A3BA5E" })
+            table.insert(result, { text = "  " .. hint, fg = "#A3BA5E" })
           end
 
           if info ~= 0 then
-            table.insert(result, { text = "  " .. info, guifg = "#7EA9A7" })
+            table.insert(result, { text = "  " .. info, fg = "#7EA9A7" })
           end
           return result
         end,
@@ -45,7 +45,7 @@ function M.config()
     },
     highlights = {
       fill = {
-        guibg = "#32302f",
+        bg = "#32302f",
       },
     },
   })
