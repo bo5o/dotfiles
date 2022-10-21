a8b8f13fc33cb65efd15676b05730508dda4465b
cbows <32486983+cbows@users.noreply.github.com>
2022-10-21 13:53:59 +0200

    zsh: use zsh-z instead of z

--- zsh/.oh-my-zsh/custom/functions.zsh
+++ zsh/.oh-my-zsh/custom/functions.zsh
@@ -1,21 +1,21 @@
 #!/usr/bin/env zsh
 
+# https://github.com/junegunn/fzf/wiki/examples#z
 # Like normal z when used with arguments but displays an fzf prompt when
 # used without.
 j() {
   if [[ -z "$*" ]]; then
-    cd "$(_z -l 2>&1 | fzf +s --tac | sed 's/^[0-9,.]* *//')"
+    cd "$(zshz -l 2>&1 | fzf +s --tac | sed 's/^[0-9,.]* *//')"
   else
     _last_z_args="$@"
-    _z "$@"
+    zshz "$@"
   fi
 }
-compctl -U -K _z_zsh_tab_completion j
 
 # Relaunch z with the arguments for the previous command
 # as the default input by using zz
 jj() {
-  cd "$(_z -l 2>&1 | sed 's/^[0-9,.]* *//' | fzf -q "$_last_z_args")"
+  cd "$(zshz -l 2>&1 | sed 's/^[0-9,.]* *//' | fzf -q "$_last_z_args")"
 }
 
 # Modified version where you can press
