cdac3c2f42de1a27c84bcd744c64035e3b9cdefe
cbows <32486983+cbows@users.noreply.github.com>
2023-02-25 22:42:08 +0100

    nvim: change style of vim.ui.select/input

--- nvim/.config/nvim/lua/plugins/dressing.lua
+++ nvim/.config/nvim/lua/plugins/dressing.lua
@@ -4,10 +4,13 @@ function M.config()
   require("dressing").setup({
     input = {
       enabled = true,
+      prompt_align = "left",
+      border = "rounded",
     },
     select = {
       enabled = true,
       backend = { "telescope" },
+      telescope = require("telescope.themes").get_cursor({}),
     },
   })
 end
