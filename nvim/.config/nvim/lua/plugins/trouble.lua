c7f77abbce7470f299ecfd8d030a19b1c96fa046
cbows <32486983+cbows@users.noreply.github.com>
2023-02-25 22:48:10 +0100

    nvim: reduce default size of trouble window

--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.config()
   require("trouble").setup({
     position = "bottom", -- position of the list can be: bottom, top, left, right
-    height = 15, -- height of the trouble list when position is top or bottom
+    height = 10, -- height of the trouble list when position is top or bottom
     width = 50, -- width of the list when position is left or right
     icons = true, -- use devicons for filenames
     mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
