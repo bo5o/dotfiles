f77ea7b3a680f20774a8a3d6006bde6ffb751777
cbows <32486983+cbows@users.noreply.github.com>
2023-02-25 02:03:15 +0100

    nvim: enable treesitter query linter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -78,6 +78,11 @@ function M.config()
       updatetime = 25,
       persist_queries = false,
     },
+    query_linter = {
+      enable = true,
+      use_virtual_text = true,
+      lint_events = { "BufWrite", "CursorHold" },
+    },
     autotag = {
       enable = true,
     },
