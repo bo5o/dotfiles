a36747eec3705d9cabf5c19103f9fcbc0068427a
cbows <32486983+cbows@users.noreply.github.com>
2023-05-11 21:11:52 +0200

    nvim: change default merge-tool layout

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -94,7 +94,7 @@ return {
           },
           merge_tool = {
             -- Config for conflicted files in diff views during a merge or rebase.
-            layout = "diff3_horizontal",
+            layout = "diff4_mixed",
             disable_diagnostics = true, -- Temporarily disable diagnostics for conflict buffers while in the view.
             winbar_info = true, -- See |diffview-config-view.x.winbar_info|
           },
