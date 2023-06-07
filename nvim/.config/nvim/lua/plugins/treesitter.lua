4d06ab2aec25f3eee061a98a302ec3e5b83cf482
cbows <32486983+cbows@users.noreply.github.com>
2023-06-07 17:42:00 +0200

    nvim: ensure comment treesitter parser is installed

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -6,6 +6,7 @@ return {
     config = function()
       require("nvim-treesitter.configs").setup({
         ensure_installed = {
+          "comment",
           "typescript",
           "javascript",
           "python",
