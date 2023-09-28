1db13331f669b9b6a213846c5a2ce12c41f975ad
bo5o <32486983+bo5o@users.noreply.github.com>
2023-09-28 18:43:54 +0200

    nvim: insert completion item only if one is selected when pressing enter

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -82,7 +82,7 @@ return {
           ["<C-e>"] = cmp.mapping({ i = cmp.mapping.close(), c = cmp.mapping.close() }),
           ["<CR>"] = cmp.mapping({
             i = function(fallback)
-              if cmp.visible() then
+              if cmp.visible() and cmp.get_active_entry() then
                 cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
               else
                 fallback()
