e5d3ecfaec283bc96d1442e1f5c27527956ae0e4
bo5o <32486983+bo5o@users.noreply.github.com>
2023-12-06 08:09:42 +0100

    nvim: add language injection for python docstrings

--- nvim/.config/nvim/after/queries/python/injections.scm
+++ nvim/.config/nvim/after/queries/python/injections.scm
@@ -4,3 +4,13 @@
     right: (string) @sql
  (#match? @sql "[sS][eE][lL][eE][cC][tT].\{-}[fF][rR][oO][mM]")
 )
+
+(function_definition
+ (block
+  (expression_statement
+   (string
+    (string_content) @injection.content (#set! injection.language "rst")
+   )
+  )
+ )
+)
