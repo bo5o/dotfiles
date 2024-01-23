d73154fbbf23657dc986bd191e7c4194ee903c70
bo5o <32486983+bo5o@users.noreply.github.com>
2024-01-24 00:02:29 +0100

    nvim: add and configure dial.nvim

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -164,6 +164,82 @@ return {
   },
 
   {
+    "monaqa/dial.nvim",
+    lazy = true,
+    keys = {
+      {
+        "<C-b>",
+        function()
+          require("dial.map").manipulate("increment", "normal")
+        end,
+      },
+      {
+        "<C-x>",
+        function()
+          require("dial.map").manipulate("decrement", "normal")
+        end,
+      },
+      {
+        "g<C-b>",
+        function()
+          require("dial.map").manipulate("increment", "gnormal")
+        end,
+      },
+      {
+        "g<C-x>",
+        function()
+          require("dial.map").manipulate("decrement", "gnormal")
+        end,
+      },
+      {
+        "<C-b>",
+        function()
+          require("dial.map").manipulate("increment", "visual")
+        end,
+        mode = "v",
+      },
+      {
+        "<C-x>",
+        function()
+          require("dial.map").manipulate("decrement", "visual")
+        end,
+        mode = "v",
+      },
+      {
+        "g<C-b>",
+        function()
+          require("dial.map").manipulate("increment", "gvisual")
+        end,
+        mode = "v",
+      },
+      {
+        "g<C-x>",
+        function()
+          require("dial.map").manipulate("decrement", "gvisual")
+        end,
+        mode = "v",
+      },
+    },
+    config = function()
+      local augend = require("dial.augend")
+      require("dial.config").augends:register_group({
+        default = {
+          augend.integer.alias.decimal,
+          augend.integer.alias.hex,
+          augend.date.alias["%Y/%m/%d"],
+          augend.date.alias["%Y-%m-%d"],
+          augend.date.alias["%m/%d"],
+          augend.date.alias["%H:%M"],
+          augend.date.alias["%d.%m.%Y"],
+          augend.date.alias["%d.%m.%y"],
+          augend.date.alias["%H:%M:%S"],
+          augend.semver.alias.semver,
+        },
+      })
+    end,
+  },
+
+  {
     "dhruvasagar/vim-table-mode",
     init = function()
       vim.g.table_mode_map_prefix = "<localleader>t"
