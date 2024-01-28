344d8ae3fe4400fa305f0cc5a4ff8b9cedae2d75
bo5o <32486983+bo5o@users.noreply.github.com>
2024-01-28 20:51:11 +0100

    nvim: update trouble.nvim configuration

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -56,18 +56,68 @@ return {
       "TroubleClose",
     },
     keys = {
-      { "<leader>qq", "<cmd>TroubleToggle<cr>", desc = "List diagnostics" },
-      { "<leader>qf", "<cmd>TroubleToggle quickfix<cr>", desc = "Toggle quickfix" },
-      { "<leader>ql", "<cmd>TroubleToggle loclist<cr>", desc = "Toggle loclist" },
       {
-        "<leader>qw",
-        "<cmd>TroubleToggle workspace_diagnostics<cr>",
+        "<leader>qq",
+        function()
+          require("trouble").toggle()
+        end,
+        desc = "List diagnostics",
+      },
+      {
+        "<leader>qf",
+        function()
+          require("trouble").toggle("quickfix")
+        end,
+        desc = "Toggle quickfix",
+      },
+      {
+        "<leader>ql",
+        function()
+          require("trouble").toggle("loclist")
+        end,
         desc = "Toggle loclist",
       },
       {
+        "<leader>qw",
+        function()
+          require("trouble").toggle("workspace_diagnostics")
+        end,
+        desc = "Show workspace diagnostics",
+      },
+      {
         "<leader>qd",
-        "<cmd>TroubleToggle document_diagnostics<cr>",
-        desc = "Toggle loclist",
+        function()
+          require("trouble").toggle("document_diagnostics")
+        end,
+        desc = "Show document diagnostics",
+      },
+      {
+        "]g",
+        function()
+          require("trouble").next({ skip_groups = true, jump = true })
+        end,
+        desc = "Jump to next trouble item",
+      },
+      {
+        "[g",
+        function()
+          require("trouble").previous({ skip_groups = true, jump = true })
+        end,
+        desc = "Jump to previous trouble item",
+      },
+      {
+        "]G",
+        function()
+          require("trouble").first({ skip_groups = true, jump = true })
+        end,
+        desc = "Jump to first trouble item",
+      },
+      {
+        "[G",
+        function()
+          require("trouble").last({ skip_groups = true, jump = true })
+        end,
+        desc = "Jump to last trouble item",
       },
     },
     opts = {
@@ -76,44 +126,56 @@ return {
       width = 50, -- width of the list when position is left or right
       icons = true, -- use devicons for filenames
       mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
+      severity = nil, -- nil (ALL) or vim.diagnostic.severity.ERROR | WARN | INFO | HINT
       fold_open = "", -- icon used for open folds
       fold_closed = "", -- icon used for closed folds
       group = true, -- group results by file
       padding = true, -- add an extra new line on top of the list
+      cycle_results = true, -- cycle item list when reaching beginning or end of list
       action_keys = { -- key mappings for actions in the trouble list
         -- map to {} to remove a mapping, for example:
         -- close = {},
         close = "q", -- close the list
         cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
         refresh = "r", -- manually refresh
-        jump = { "<cr>", "<tab>" }, -- jump to the diagnostic or open / close folds
+        jump = { "<cr>", "<tab>", "<2-leftmouse>" }, -- jump to the diagnostic or open / close folds
         open_split = { "<c-x>" }, -- open buffer in new split
         open_vsplit = { "<c-v>" }, -- open buffer in new vsplit
         open_tab = { "<c-t>" }, -- open buffer in new tab
         jump_close = { "o" }, -- jump to the diagnostic and close the list
         toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
+        switch_severity = "s", -- switch "diagnostics" severity filter level to HINT / INFO / WARN / ERROR
         toggle_preview = "P", -- toggle auto_preview
         hover = "K", -- opens a small popup with the full multiline message
         preview = "p", -- preview the diagnostic location
+        open_code_href = "c", -- if present, open a URI with more information about the diagnostic error
         close_folds = { "zM", "zm" }, -- close all folds
         open_folds = { "zR", "zr" }, -- open all folds
         toggle_fold = { "zA", "za" }, -- toggle fold of current file
-        previous = "k", -- preview item
+        previous = "k", -- previous item
         next = "j", -- next item
+        help = "?", -- help menu
       },
+      multiline = true, -- render multi-line messages
       indent_lines = true, -- add an indent guide below the fold icons
+      win_config = { border = "single" }, -- window configuration for floating windows. See |nvim_open_win()|.
       auto_open = false, -- automatically open the list when you have diagnostics
       auto_close = false, -- automatically close the list when you have no diagnostics
       auto_preview = false, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
       auto_fold = false, -- automatically fold a file trouble list at creation
       auto_jump = { "lsp_definitions" }, -- for the given modes, automatically jump if there is only a single result
+      include_declaration = {
+        "lsp_references",
+        "lsp_implementations",
+        "lsp_definitions",
+      }, -- for the given modes, include the declaration of the current symbol in the results
       signs = {
         -- icons / text used for a diagnostic
-        error = "",
-        warning = "",
-        hint = "",
+        error = "",
+        warning = "",
+        hint = "",
         information = "",
-        other = "﫠",
+        other = "",
       },
       use_diagnostic_signs = false, -- enabling this will use the signs defined in your lsp client
     },
