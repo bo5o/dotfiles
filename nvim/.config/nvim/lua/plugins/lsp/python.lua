630d56f469b5945c77a64dbc866be7e2a1fbcfd4
bo5o <32486983+bo5o@users.noreply.github.com>
2024-05-28 01:38:39 +0200

    nvim: use pyright and ruff as python language servers

--- nvim/.config/nvim/lua/plugins/lsp/python.lua
+++ nvim/.config/nvim/lua/plugins/lsp/python.lua
@@ -1,41 +1,11 @@
 local M = {}
 
 function M.setup()
-  require("lspconfig").pylsp.setup({
+  require("lspconfig").ruff.setup({})
+  require("lspconfig").basedpyright.setup({
     settings = {
-      pylsp = {
-        configurationSources = { "flake8" },
-        plugins = {
-          jedi_completion = { enabled = true },
-          jedi_definition = { enabled = true },
-          jedi_hover = { enabled = true },
-          jedi_references = { enabled = true },
-          jedi_signature_help = { enabled = true },
-          jedi_symbols = { enabled = true },
-          black = { enabled = true },
-          ruff = {
-            enabled = true,
-            select = { "E", "F", "I" },
-          },
-          mypy = {
-            enabled = true,
-            dmypy = true,
-            strict = true,
-            report_progress = true,
-          },
-          autopep8 = { enabled = false },
-          flake8 = { enabled = false },
-          isort = { enabled = false },
-          preload = { enabled = true },
-          mccabe = { enabled = false },
-          pycodestyle = { enabled = false },
-          pydocstyle = { enabled = false },
-          pyflakes = { enabled = false },
-          pylint = { enabled = false },
-          rope_completion = { enabled = false },
-          rope_autoimport = { enabled = false },
-          yapf = { enabled = false },
-        },
+      pyright = {
+        disableOrganizeImports = true,
       },
     },
   })
