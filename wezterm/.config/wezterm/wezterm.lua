7731a7cf65eca61c241ada89ac091353ad1a1f8d
bo5o <32486983+bo5o@users.noreply.github.com>
2024-06-03 11:27:32 +0200

    wezterm: use WebGpu frontend

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,13 +3,14 @@ local wezterm = require("wezterm")
 return {
   font = wezterm.font("Iosevka Fixed SS14"),
   font_size = 12,
-  line_height = 0.95,
+  line_height = 1.00,
   color_scheme = "Gruvbox dark, medium (base16)",
+  warn_about_missing_glyphs = false,
   enable_scroll_bar = false,
   enable_tab_bar = true,
   hide_tab_bar_if_only_one_tab = true,
-  anti_alias_custom_block_glyphs = false,
   window_decorations = "RESIZE",
+  front_end = "WebGpu",
   window_padding = {
     left = 0,
     right = 0,
