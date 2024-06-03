b34d4a922a65062f7d52b7ca3168f17a780d1d2d
bo5o <32486983+bo5o@users.noreply.github.com>
2024-06-03 11:20:07 +0200

    wezterm: reformat lua configuration

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,19 +1,19 @@
 local wezterm = require("wezterm")
 
 return {
-	font = wezterm.font("Iosevka Fixed SS14"),
-	font_size = 12,
-	line_height = 0.95,
-	color_scheme = "Gruvbox dark, medium (base16)",
-	enable_scroll_bar = false,
-	enable_tab_bar = true,
-	hide_tab_bar_if_only_one_tab = true,
-	anti_alias_custom_block_glyphs = false,
-	window_decorations = "RESIZE",
-	window_padding = {
-		left = 0,
-		right = 0,
-		top = 0,
-		bottom = 0,
-	},
+  font = wezterm.font("Iosevka Fixed SS14"),
+  font_size = 12,
+  line_height = 0.95,
+  color_scheme = "Gruvbox dark, medium (base16)",
+  enable_scroll_bar = false,
+  enable_tab_bar = true,
+  hide_tab_bar_if_only_one_tab = true,
+  anti_alias_custom_block_glyphs = false,
+  window_decorations = "RESIZE",
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
 }
