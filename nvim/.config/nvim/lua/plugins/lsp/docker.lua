1de7f0abadd0464664cd09799470e43bc3864028
bo5o <32486983+bo5o@users.noreply.github.com>
2024-06-06 15:10:02 +0200

    nvim: disable docker_compose_language_service for now (use yamlls instead)

--- nvim/.config/nvim/lua/plugins/lsp/docker.lua
+++ nvim/.config/nvim/lua/plugins/lsp/docker.lua
@@ -2,7 +2,13 @@ local M = {}
 
 function M.setup()
   require("lspconfig").dockerls.setup({ filetypes = { "Dockerfile", "dockerfile" } })
-  require("lspconfig").docker_compose_language_service.setup({})
+  -- require("lspconfig").docker_compose_language_service.setup({
+  --   settings = {
+  --     telemetry = {
+  --       enableTelemetry = false,
+  --     },
+  --   },
+  -- })
 end
 
 return M
