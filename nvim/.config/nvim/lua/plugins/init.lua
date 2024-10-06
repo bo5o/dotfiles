e8bd1409927b05672e85bb132912ccbba7004e61
bo5o <32486983+bo5o@users.noreply.github.com>
2024-10-06 18:00:47 +0200

    nvim: add type hints for indent-blankline configuration

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -345,6 +345,8 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     event = { "BufReadPost", "BufNewFile" },
     main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
     opts = {
       indent = { char = "┊" },
       scope = {
