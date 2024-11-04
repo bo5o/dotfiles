1190ead14ef362563eda9442ed36b0c82b1946aa
bo5o <32486983+bo5o@users.noreply.github.com>
2024-11-04 20:19:30 +0100

    nvim: configure typescript language server

--- nvim/.config/nvim/lua/plugins/lsp/typescript.lua
+++ nvim/.config/nvim/lua/plugins/lsp/typescript.lua
@@ -1,5 +1,22 @@
 local M = {}
 
-function M.setup() end
+function M.setup()
+  require("lspconfig").ts_ls.setup({
+    init_options = {
+      plugins = {
+        {
+          name = "@vue/typescript-plugin",
+          location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
+          languages = { "javascript", "typescript", "vue" },
+        },
+      },
+    },
+    filetypes = {
+      "javascript",
+      "typescript",
+      "vue",
+    },
+  })
+end
 
 return M
