cc0bd0435e8d143248724aa6c21344f81dbed7bb
bo5o <32486983+bo5o@users.noreply.github.com>
2025-02-02 16:56:55 +0100

    nvim: improve commenting

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -213,4 +213,12 @@ return {
       )
     end,
   },
+
+  -- Comments
+  {
+    "folke/ts-comments.nvim",
+    opts = {},
+    event = "VeryLazy",
+    enabled = vim.fn.has("nvim-0.10.0") == 1,
+  },
 }
