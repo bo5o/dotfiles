35e77126d8dbd14c7a5957f42a1a4c955ddc9c36
bo5o <32486983+bo5o@users.noreply.github.com>
2025-02-06 19:32:59 +0100

    wezterm: disable audible bell and use larger icons

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -17,7 +17,7 @@ end)
 return {
   font = wezterm.font_with_fallback({
     "Iosevka Fixed SS14",
-    { family = "Symbols Nerd Font Mono", scale = 0.75 }
+    { family = "Symbols Nerd Font Mono", scale = 0.90 }
   }),
   font_size = 12,
   line_height = 1.00,
@@ -28,6 +28,7 @@ return {
   hide_tab_bar_if_only_one_tab = true,
   window_decorations = "RESIZE",
   front_end = "WebGpu",
+  audible_bell = "Disabled",
   window_padding = {
     left = 0,
     right = 0,
