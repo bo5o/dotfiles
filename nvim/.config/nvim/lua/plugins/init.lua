1d99adf1bd83da64e987ecdda065bbc7a870ee00
bo5o <32486983+bo5o@users.noreply.github.com>
2025-02-09 01:32:13 +0100

    nvim: remove oil.nvim

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -583,41 +583,6 @@ return {
   },
 
   {
-    "stevearc/oil.nvim",
-    keys = {
-      { "<leader>oo", "<cmd>Oil<cr>", desc = "Open parent directory in oil" },
-    },
-    ---@module 'oil'
-    ---@type oil.SetupOpts
-    opts = {
-      keymaps = {
-        ["g?"] = { "actions.show_help", mode = "n" },
-        ["<CR>"] = "actions.select",
-        ["L"] = "actions.select",
-        ["<C-s>"] = { "actions.select", opts = { vertical = true } },
-        ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
-        ["<C-t>"] = { "actions.select", opts = { tab = true } },
-        ["<C-p>"] = "actions.preview",
-        ["<C-c>"] = { "actions.close", mode = "n" },
-        ["<C-l>"] = "actions.refresh",
-        ["H"] = { "actions.parent", mode = "n" },
-        ["_"] = { "actions.open_cwd", mode = "n" },
-        ["`"] = { "actions.cd", mode = "n" },
-        ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
-        ["gs"] = { "actions.change_sort", mode = "n" },
-        ["gx"] = "actions.open_external",
-        ["zh"] = { "actions.toggle_hidden", mode = "n" },
-        ["g\\"] = { "actions.toggle_trash", mode = "n" },
-      },
-      default_file_explorer = true,
-      delete_to_trash = true,
-      lsp_file_methods = {
-        autosave_changes = true,
-      },
-    },
-  },
-
-  {
     "kyazdani42/nvim-tree.lua",
     version = "*",
     lazy = false,
