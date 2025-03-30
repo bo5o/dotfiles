8097ac1e515fe698acf23720aa7200a8aa8739ab
bo5o <32486983+bo5o@users.noreply.github.com>
2025-03-30 18:37:04 +0200

    nvim: fix mise.toml treesitter predicate

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -120,7 +120,7 @@ return {
       require("vim.treesitter.query").add_predicate("is-mise?", function(_, _, bufnr, _)
         local filepath = vim.api.nvim_buf_get_name(tonumber(bufnr) or 0)
         local filename = vim.fn.fnamemodify(filepath, ":t")
-        return string.match(filename, ".*mise.*%.toml$")
+        return string.match(filename, ".*mise.*%.toml$") ~= nil
       end, { force = true, all = false })
     end,
   },
