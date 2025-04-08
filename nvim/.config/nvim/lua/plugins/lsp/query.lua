9c7bf3f207838b81e621fdd674518bfa56ed0bd2
bo5o <32486983+bo5o@users.noreply.github.com>
2025-04-08 21:17:07 +0200

    nvim: update ts_query_ls config

--- nvim/.config/nvim/lua/plugins/lsp/query.lua
+++ nvim/.config/nvim/lua/plugins/lsp/query.lua
@@ -6,6 +6,9 @@ function M.setup()
       parser_install_directories = {
         vim.fs.joinpath(vim.fn.stdpath("data"), "/lazy/nvim-treesitter/parser/"),
       },
+      language_retrieval_patterns = {
+        "languages/src/([^/]+)/[^/]+\\.scm$",
+      },
     },
   })
 end
