5939d889a277e19a138c1d989b5815bbbeee8c63
bo5o <32486983+bo5o@users.noreply.github.com>
2025-04-13 16:13:35 +0200

    nvim: move diagnostic config into settings

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -114,26 +114,6 @@ return {
           },
         })
 
-      vim.diagnostic.config({
-        virtual_text = false,
-        severity_sort = true,
-        signs = {
-          text = {
-            [vim.diagnostic.severity.ERROR] = "",
-            [vim.diagnostic.severity.WARN] = "",
-            [vim.diagnostic.severity.INFO] = "",
-            [vim.diagnostic.severity.HINT] = "",
-          },
-          numhl = {
-            [vim.diagnostic.severity.WARN] = "DiagnosticWarn",
-            [vim.diagnostic.severity.ERROR] = "DiagnosticError",
-            [vim.diagnostic.severity.INFO] = "DiagnosticInfo",
-            [vim.diagnostic.severity.HINT] = "DiagnosticHint",
-          },
-        },
-        float = {
-          border = "rounded",
-          source = true,
         },
       })
     end,
--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -175,3 +175,26 @@ vim.lsp.enable({
   "vtsls",
   "yamlls",
 })
+
+vim.diagnostic.config({
+  virtual_text = false,
+  severity_sort = true,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.INFO] = "",
+      [vim.diagnostic.severity.HINT] = "",
+    },
+    numhl = {
+      [vim.diagnostic.severity.WARN] = "DiagnosticWarn",
+      [vim.diagnostic.severity.ERROR] = "DiagnosticError",
+      [vim.diagnostic.severity.INFO] = "DiagnosticInfo",
+      [vim.diagnostic.severity.HINT] = "DiagnosticHint",
+    },
+  },
+  float = {
+    border = "rounded",
+    source = true,
+  },
+})
