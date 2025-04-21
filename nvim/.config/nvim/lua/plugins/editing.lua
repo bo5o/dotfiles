e0c66ee51a1e651b69eb5674156a104636abbdc8
bo5o <32486983+bo5o@users.noreply.github.com>
2025-04-22 00:12:30 +0200

    nvim: add csvview

--- nvim/.config/nvim/lua/plugins/editing.lua
+++ nvim/.config/nvim/lua/plugins/editing.lua
@@ -107,4 +107,20 @@ return {
       vim.api.nvim_command("EmmetInstall")
     end,
   },
+
+  {
+    "hat0uma/csvview.nvim",
+    cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
+    ---@module "csvview"
+    ---@type CsvView.Options
+    opts = {
+      parser = { comments = { "#", "//" } },
+      keymaps = {
+        textobject_field_inner = { "if", mode = { "o", "x" } },
+        textobject_field_outer = { "af", mode = { "o", "x" } },
+        jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+        jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+      },
+    },
+  },
 }
