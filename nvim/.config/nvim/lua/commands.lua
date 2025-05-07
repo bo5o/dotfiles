0a9854f60b3228ecccd3e58d6c717186ec36fc1c
bo5o <32486983+bo5o@users.noreply.github.com>
2025-05-08 00:34:19 +0200

    nvim: enable quit aerial nav with q

--- nvim/.config/nvim/lua/commands.lua
+++ nvim/.config/nvim/lua/commands.lua
@@ -74,6 +74,7 @@ vim.api.nvim_create_autocmd("FileType", {
     "qf",
     "startuptime",
     "tsplayground",
+    "aerial-nav",
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
