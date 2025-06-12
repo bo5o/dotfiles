db51d7d14f490a54ad3fbe958585a01d43c18cc4
bo5o <32486983+bo5o@users.noreply.github.com>
2025-06-12 11:36:28 +0200

    nvim: remove python docstring treesitter injection
    did not work well with indented docstrings

--- nvim/.config/nvim/after/queries/python/injections.scm
+++ nvim/.config/nvim/after/queries/python/injections.scm
@@ -4,11 +4,3 @@
   (#match? @injection.content
     "^\\s*(SELECT|select|INSERT|insert|UPDATE|update|DELETE|delete).+(FROM|from|INTO|into|VALUES|values|SET|set)")
   (#set! injection.language "sql"))
-
-(function_definition
-  (block
-    (expression_statement
-      (string
-        (string_content) @injection.content
-        (#set! injection.language "rst")
-        (#set! injection.indent 4)))))
