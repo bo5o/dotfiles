3875d65bd5ecc18c59ab3b8643a3795864270afe
bo5o <32486983+bo5o@users.noreply.github.com>
2025-12-15 09:15:36 +0100

    nvim: unpin codecompanion plugins

--- nvim/.config/nvim/lua/plugins/ai.lua
+++ nvim/.config/nvim/lua/plugins/ai.lua
@@ -1,7 +1,6 @@
 return {
   {
     "ravitemer/mcphub.nvim",
-    commit = "8ff40b5edc649959bb7e89d25ae18e055554859a", -- wait for codecompanion v18.0.0
     keys = {
       { "<leader>om", "<cmd>MCPHub<cr>", desc = "Open MCP hub" },
     },
@@ -20,7 +19,7 @@ return {
 
   {
     "olimorris/codecompanion.nvim",
-    version = "^17.0.0", -- TODO: bump to 18
+    version = "^18.0.0",
     cmd = {
       "CodeCompanion",
       "CodeCompanionChat",
@@ -28,10 +27,7 @@ return {
     },
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
-      {
-        "ravitemer/codecompanion-history.nvim",
-        commit = "eb99d256352144cf3b6a1c45608ec25544a0813d", -- wait for codecompanion v18.0.0
-      },
+      "ravitemer/codecompanion-history.nvim",
     },
     keys = {
       {
