41937ff08379f306713c5ae35d7fe8349b0b2f56
bo5o <32486983+bo5o@users.noreply.github.com>
2025-12-23 15:59:20 +0100

    zsh: update tmux alias

--- zsh/.oh-my-zsh/custom/aliases.zsh
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -99,7 +99,7 @@ alias sdn="sudo shutdown -h now"
 alias sm="~/bin/syncmail.sh"
 alias spot="ncspot"
 alias stow='stow -v'
-alias t="tmux attach || tmux new-session"
+alias to="tmux new-session -A -s"
 alias ta="tmux attach -t"
 alias ternimal-anaconda="ternimal length=100 thickness=1,4,1,0,0 radius=6,12 gradient=0:#666600,0.5:#00ff00,1:#003300"
 alias ternimal-rainbow="ternimal length=20 thickness=70,15,0,1,0 padding=10 radius=5 gradient=0.03:#ffff00,0.15:#0000ff,0.3:#ff0000,0.5:#00ff00"
