b3c6c113d6529b9a85a2e01db5cc20f643250cbf
bo5o <32486983+bo5o@users.noreply.github.com>
2026-03-08 22:19:35 +0100

    nvim: add bash syntax injection for shell script shebangs in YAML

--- nvim/.config/nvim/after/queries/yaml/injections.scm
+++ nvim/.config/nvim/after/queries/yaml/injections.scm
@@ -7,3 +7,39 @@
     (block_scalar) @injection.content
     (#set! injection.language "dockerfile")
     (#offset! @injection.content 0 1 0 0)))
+
+(block_mapping_pair
+  key: (flow_node) @_content
+  (#eq? @_content "content")
+  value: (block_node
+    (block_scalar) @injection.content
+    (#contains? @injection.content "#!/bin/sh")
+    (#set! injection.language "bash")
+    (#offset! @injection.content 0 1 0 0)))
+
+(block_mapping_pair
+  key: (flow_node) @_content
+  (#eq? @_content "content")
+  value: (block_node
+    (block_scalar) @injection.content
+    (#contains? @injection.content "#!/bin/bash")
+    (#set! injection.language "bash")
+    (#offset! @injection.content 0 1 0 0)))
+
+(block_mapping_pair
+  key: (flow_node) @_content
+  (#eq? @_content "content")
+  value: (block_node
+    (block_scalar) @injection.content
+    (#contains? @injection.content "#!/usr/bin/env sh")
+    (#set! injection.language "bash")
+    (#offset! @injection.content 0 1 0 0)))
+
+(block_mapping_pair
+  key: (flow_node) @_content
+  (#eq? @_content "content")
+  value: (block_node
+    (block_scalar) @injection.content
+    (#contains? @injection.content "#!/usr/bin/env bash")
+    (#set! injection.language "bash")
+    (#offset! @injection.content 0 1 0 0)))
